a1071e4b2dac8a23fcdc598565cd3e628683434e
phmmdev <phmmdev@gmail.com>
2021-03-15 07:43:45 -0300

    adding jsp examples and doing some tests

--- spring-boot-first-web-application/src/main/java/com/knowledge/springboot/web/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/knowledge/springboot/web/controller/LoginController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController
 {
     @RequestMapping("/login")
-    @ResponseBody
-    public String sayHello()
+    public String loginMessage()
     {
-        return "Hello World Customized";
+        return "login";
     }
 }
---
+++ spring-boot-first-web-application/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,6 @@
+<html>
+<title></title>
+<body>
+    First JSP
+</body>
+</html>
