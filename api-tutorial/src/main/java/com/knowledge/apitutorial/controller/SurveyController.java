e155acf3cf66b9c1abd85b828dd3a2f0bd33d4fe
phmmdev <phmmdev@gmail.com>
2021-04-10 20:17:53 -0300

    adding post method

--- api-tutorial/src/main/java/com/knowledge/apitutorial/controller/SurveyController.java
+++ api-tutorial/src/main/java/com/knowledge/apitutorial/controller/SurveyController.java
@@ -4,11 +4,15 @@ import com.knowledge.apitutorial.model.Question;
 import com.knowledge.apitutorial.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.swing.text.html.Option;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class SurveyController
@@ -21,5 +25,25 @@ public class SurveyController
     public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId)
     {
         return surveyService.retrieveQuestions(surveyId);
+    }
+
+    @GetMapping("/surveys/{surveyId}/questions/{questionId}")
+    public Question retrieveDetailsFromQuestion(@PathVariable String surveyId,
+                                                     @PathVariable String questionId)
+    {
+        return surveyService.retrieveQuestion(surveyId, questionId);
+    }
+
+    @PostMapping("/surveys/{surveyId}/questions")
+    public ResponseEntity<Question> addQuestion(@PathVariable String surveyId, @RequestBody Question question)
+    {
+        Question createdQuestion = surveyService.addQuestion(surveyId, question);
+        if(createdQuestion == null)
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(createdQuestion.getId()).toUri();
+
+        return ResponseEntity.created(location).body(createdQuestion);
     }
 }
