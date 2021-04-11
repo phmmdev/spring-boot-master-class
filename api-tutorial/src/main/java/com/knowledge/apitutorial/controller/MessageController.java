c24ab9b1133b10a9fede39c6add3bb31daf0abd4
phmmdev <phmmdev@gmail.com>
2021-04-11 06:55:44 -0300

    working with profiles

--- api-tutorial/src/main/java/com/knowledge/apitutorial/controller/MessageController.java
+++ api-tutorial/src/main/java/com/knowledge/apitutorial/controller/MessageController.java
@@ -3,15 +3,18 @@ package com.knowledge.apitutorial.controller;
 import com.knowledge.apitutorial.service.MessageService;
 import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("localhost")
 @RestController
 public class MessageController
 {
     @Autowired
     MessageService messageService;
+
 
     @GetMapping("/welcome")
     public String showsWelcomeMessage()
